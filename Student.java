a47b398bb87ecc994cc85e8e0eceadb9bcd17321
pavan pankhade <pavanpankhade95@gmail.com>
2025-12-22 22:57:28 +0530

    Student.java
    - Created Student class
    - Defined student attributes and constructors
    - Added methods to display and manage student details
    - Improved code readability and structure

---
+++ Student.java
@@ -0,0 +1,11 @@
+package blcprogram;
+
+public class Student {
+	public static String getDetails(int roll,String name,double Fees) {
+		return "[Student name is:"+name+",roll is:"+roll+",fees is:"+Fees+"]";
+		
+		
+		
+	}
+
+}
